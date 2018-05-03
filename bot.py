eb29d12106ad6f05113f8ac36cfd4ffe885ecf8b
Luis Garcia Calzada <luisxciv@gmail.com>
2018-05-03 16:58:38 -0500

    rmv

--- bot.py
+++ bot.py
@@ -11,18 +11,6 @@ from google.cloud.language import enums
 from google.cloud.language import types as lanuage_types
 from googleapiclient.discovery import build
 
-white = '\033[1;97m'
-green = '\033[1;32m'
-red = '\033[1;31m'
-yellow = '\033[1;33m'
-end = '\033[1;m'
-important = '\033[35m[*]\033[1;m '
-info = '\033[1;33m[!]\033[1;m '
-que = '\033[1;34m[?]\033[1;m '
-bad = '\033[1;31m[-]\033[1;m '
-good = '\033[1;32m[+]\033[1;m '
-run = '\033[1;97m[~]\033[1;m '
-
 hardreturn = '\n'
 
 HQValues = ['HQ', 'HO', 'H0']
