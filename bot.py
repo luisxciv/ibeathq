d227b85ac7448e30f4f026e9a91baecd69342c60
Luis Garcia Calzada <luisxciv@gmail.com>
2018-05-03 12:30:45 -0500

    google dependencies

--- bot.py
+++ bot.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python2.7
+
+import urllib2
+from google.auth import exceptions
+from google.cloud import vision
+from google.cloud.vision import types
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types as lanuage_types
+from googleapiclient.discovery import build
+
 white = '\033[1;97m'
 green = '\033[1;32m'
 red = '\033[1;31m'
