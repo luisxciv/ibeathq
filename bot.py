15b452bac2777e80231024e02217a1038e6295e2
Luis Garcia Calzada <luisxciv@gmail.com>
2018-05-11 11:24:41 -0500

    declare color values

--- bot.py
+++ bot.py
@@ -15,7 +15,18 @@ from googleapiclient.discovery import build
 
 
 hardreturn = '\n'
-
+white = '\033[1;97m'
+green = '\033[1;32m'
+red = '\033[1;31m'
+yellow = '\033[1;33m'
+end = '\033[1;m'
+important = '\033[35m[*]\033[1;m '
+hardreturn = '\n'
+info = '\033[1;33m[!]\033[1;m '
+que = '\033[1;34m[?]\033[1;m '
+bad = '\033[1;31m[-]\033[1;m '
+good = '\033[1;32m[+]\033[1;m '
+run = '\033[1;97m[~]\033[1;m '
 HQValues = ['HQ', 'HO', 'H0']
 bad = '\033[1;31m[-]\033[1;m '
 
