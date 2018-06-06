9fa7ab5390c3306acacd47267c37a906764818a0
Luis Garcia Calzada <luisxciv@gmail.com>
2018-06-06 14:45:32 -0500

    Final
    display
    indentation
    and working final product.

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python2.7
-
 import io
 import os
 import six
@@ -18,116 +17,131 @@ from google.cloud.language import enums
 from google.cloud.language import types as lanuage_types
 from googleapiclient.discovery import build
 
+from time import strftime, gmtime
 
-hardreturn = '\n'
 white = '\033[1;97m'
+ICyan = "\033[0;96m"
+Blue = "\033[0;34m"
 green = '\033[1;32m'
 red = '\033[1;31m'
 yellow = '\033[1;33m'
+BIPurple = "\033[1;92m"
+BPurple = "\033[0;92m"
+On_White = "\033[47m"
 end = '\033[1;m'
 important = '\033[35m[*]\033[1;m '
 hardreturn = '\n'
-info = '\033[1;33m[!]\033[1;m '
+info = '\033[1;92m[!]\033[1;m '
 que = '\033[1;34m[?]\033[1;m '
 bad = '\033[1;31m[-]\033[1;m '
-good = '\033[1;32m[+]\033[1;m '
+good = '\033[0;92m[+]\033[1;m '
 run = '\033[1;97m[~]\033[1;m '
+
 HQValues = ['HQ', 'HO', 'H0']
-bad = '\033[1;31m[-]\033[1;m '
 
-def get_picture_blocks(path):
 
+def get_blocks(path):
     client = vision.ImageAnnotatorClient()
 
     with io.open(path, 'rb') as image_file:
         content = image_file.read()
 
-    image = types.image(content=content)
+    image = types.Image(content=content)
 
-    response = client.document_text_detention(image=image)
-    document = response.full_text_anotaion
+    response = client.document_text_detection(image=image)
+    document = response.full_text_annotation
 
     blocks = {}
-#For loop to separate words into blocks and define a sentence
+
     for page in document.pages:
         block_number = 1
         for block in page.blocks:
-            block_words = 1
-        for paragraph in block.paragraph:
-            block_words.extend(paragraph.words)
+            block_words = []
+            for paragraph in block.paragraphs:
+                block_words.extend(paragraph.words)
 
             word_text = []
-            for block in block_words:
+            for word in block_words:
                 symbols_text = ''
-                for symbol in block.symbols:
+                for symbol in word.symbols:
                     symbols_text = symbols_text + symbol.text
-                    word_text.append(symbols_text.strip())
+                word_text.append(symbols_text.strip())
+
             sentence = ' '.join(word_text)
             if sentence.strip().endswith(' ?'):
                 sentence = sentence.replace(' ?', '?')
             blocks[block_number] = sentence
             block_number += 1
 
-        return blocks
-###
-def discover(text):
-#google cloud language API integration
+    if args.verbose:
+        print(important + 'Found %s blocks' % block_number + hardreturn)
+
+    return blocks
+
+
+def get_entity(text):
+    """Detects entities in the text."""
     client = language.LanguageServiceClient()
 
     if isinstance(text, six.binary_type):
         text = text.decode('utf-8')
 
-        document = lanuage_types.Document(
-            content=text,
-            type=enums.Document.Type.PLAIN_TEXT)
-        entities = client.analyze_entities(document).entities
-#analyze the blocks with the learning engine
-        try:
-            results = ' '.join(
-                [entities[0].name, entities[1].name, entities[2].name])
-        except IndexError:
-            try:
-                results = ' '.join([entities[0].name, entities[1].name])
-            except IndexError:
-                results = entities[0].name
+    # Instantiates a plain text document.
+    document = lanuage_types.Document(
+        content=text,
+        type=enums.Document.Type.PLAIN_TEXT)
 
-        return results
-#Read question
-def read_question(blocks):
-        for key, value in blocks.iteritems():
-            for HQ in HQValues:
-                if HQ in value.upper().strip() and blocks[key + 1].endswith('?'):
+    entities = client.analyze_entities(document).entities
+
+    try:
+        results = ' '.join(
+            [entities[0].name, entities[1].name, entities[2].name])
+    except IndexError:
+        try:
+            results = ' '.join([entities[0].name, entities[1].name])
+        except IndexError:
+            results = entities[0].name
+
+
+    return results
+
+
+def get_question(blocks):
+    for key, value in blocks.iteritems():
+        for HQ in HQValues:
+            if HQ in value.upper().strip() and blocks[key + 1].endswith('?'):
+                return blocks[key + 1]
+            try:
+                test = int(value)
+                if test > 0 and blocks[key + 1].endswith('?'):
                     return blocks[key + 1]
-                try:
-                    test = int(value)
-                    if test > 0 and blocks[key + 1].endswith('?'):
-                        return blocks[key + 1]
-                    if test > 0 and blocks[key + 2].endswith('?'):
-                        return blocks[key + 2]
-                    if test > 0 and blocks[key + 3].endswith('?'):
-                        return blocks[key + 3]
-                except ValueError, KeyError:
-                    pass
-                try:
-                    if 'K' in value.upper().strip() and blocks[key + 1].endswith('?'):
-                        return blocks[key + 1]
-                    if 'K' in value.upper().strip() and blocks[key + 2].endswith('?'):
-                        return blocks[key + 2]
-                    if 'K' in value.upper().strip() and blocks[key + 3].endswith('?'):
-                        return blocks[key + 3]
-                except ValueError, KeyError:
-                    pass
-                try:
-                    if 'M' in value.upper().strip() and blocks[key + 1].endswith('?'):
-                        return blocks[key + 1]
-                    if 'M' in value.upper().strip() and blocks[key + 2].endswith('?'):
-                        return blocks[key + 2]
-                    if 'M' in value.upper().strip() and blocks[key + 3].endswith('?'):
-                        return blocks[key + 3]
-                except ValueError, KeyError:
-                    pass
-                print(bad + "Error getting question, defaulting")
-                return blocks[3]
+                if test > 0 and blocks[key + 2].endswith('?'):
+                    return blocks[key + 2]
+                if test > 0 and blocks[key + 3].endswith('?'):
+                    return blocks[key + 3]
+            except ValueError, KeyError:
+                pass
+            try:
+                if 'K' in value.upper().strip() and blocks[key + 1].endswith('?'):
+                    return blocks[key + 1]
+                if 'K' in value.upper().strip() and blocks[key + 2].endswith('?'):
+                    return blocks[key + 2]
+                if 'K' in value.upper().strip() and blocks[key + 3].endswith('?'):
+                    return blocks[key + 3]
+            except ValueError, KeyError:
+                pass
+            try:
+                if 'M' in value.upper().strip() and blocks[key + 1].endswith('?'):
+                    return blocks[key + 1]
+                if 'M' in value.upper().strip() and blocks[key + 2].endswith('?'):
+                    return blocks[key + 2]
+                if 'M' in value.upper().strip() and blocks[key + 3].endswith('?'):
+                    return blocks[key + 3]
+            except ValueError, KeyError:
+                pass
+    print(bad + "Error getting question, defaulting")
+    return blocks[3]
+
 
 def get_answers(question, blocks):
     try:
@@ -140,8 +154,9 @@ def get_answers(question, blocks):
         print(bad + str(e))
         return [blocks[4], blocks[5], blocks[6]]
 
+
 def search_entity(service, question, answers, queue):
-    question = discover(question)
+    question = get_entity(question)
     return search(service, question, answers, queue)
 
 
@@ -162,16 +177,19 @@ def search(service, question, answers, queue):
 
 
 def choose(answer):
-    print(good + 'Choose ' + green + answer + end)
-#
+    print(good + 'Choose ' + BIPurple + answer + end)
+
+
 
 def print_answers(num, answer, count):
-    print(good + 'Answer %s: %s' % (str(num), answer) + green +
-          ' Found: ' + str(count) + ' results' + end)
+    print(good + 'Option %s: %s' % (str(num), answer) + BPurple + " " + str(count) + ' results' + end)
+
 
 def find_answer(path):
-    blocks = get_picture_blocks(path)
-    question = read_question(blocks)
+    if args.verbose:
+        print(run + 'Filename: ' + path)
+    blocks = get_blocks(path)
+    question = get_question(blocks)
     print(que + 'Question: ' + question + hardreturn)
     answers = get_answers(question, blocks)
     service = build("customsearch", "v1",
@@ -182,70 +200,70 @@ def find_answer(path):
     keywords = question.split()
 
     try:
-        question2 = ''.join([keywords[-1], keywords[-2], keywords[-3]])
+        question2 = ' '.join([keywords[-1], keywords[-2], keywords[-3]])
     except IndexError:
         try:
-            question2 = ''.join([keywords[-1], keywords[-2]])
+            question2 = ' '.join([keywords[-1], keywords[-2]])
         except IndexError:
             try:
                 question2 = keywords[-1]
             except IndexError:
+                if args.verbose:
+                    print(bad + 'Could not find key words')
 
+    results2_queue = Queue.Queue()
+    search2 = threading.Thread(target=search, args=(
+        service, question2, answers, results2_queue))
+    search2.start()
 
-                ###here we start searching for answers and selecting the most common ones
-                results2_queue = Queue.Queue()
-                search2 = threading.Thread(target=search, args=(
-                    service, question2, answers, results2_queue))
-                search2.start()
+    search3 = threading.Thread(target=search_entity, args=(
+        service, question, answers, results2_queue))
+    search3.start()
 
-                search3 = threading.Thread(target=search_entity, args=(
-                    service, question, answers, results2_queue))
-                search3.start()
+    items = json.dumps(results1['items']).lower()
+    answer1count = items.count(answers[0].lower())
+    answer2count = items.count(answers[1].lower())
+    answer3count = items.count(answers[2].lower())
 
-                items = json.dumps(results1['items']).lower()
-                countanswer1 = items.count(answers[1].lower())
-                countanswer2 = items.count(answers[2].lower())
-                countanswer3 = items.count(answers[3].lower())
+    print(info + 'Search results:')
+    print_answers(1, answers[0], answer1count)
+    print_answers(2, answers[1], answer2count)
+    print_answers(3, answers[2], answer3count)
+    if answer1count == answer2count and answer2count == answer3count:
 
-                print(info + 'Google Answers:')
-                print_answers(1, answers[0], countanswer1)
-                print_answers(2, answers[1], countanswer2)
-                print_answers(3, answers[2], countanswer3)
-                if countanswer1 == countanswer2 and countanswer2 == countanswer3:
-                    search2.join()
-                    search3.join()
-                    results2 = results2_queue.get()
+        search2.join()
+        search3.join()
+        results2 = results2_queue.get()
 
-                    items2 = json.dumps(results2['items']).lower()
+        items2 = json.dumps(results2['items']).lower()
 
-                    countanswer1 = items.count(answers[0].lower())
-                    countanswer2 = items.count(answers[0].lower())
-                    countanswer3 = items.count(answers[0].lower())
+        answer1count = items.count(answers[0].lower())
+        answer2count = items.count(answers[1].lower())
+        answer3count = items.count(answers[2].lower())
 
-                    if countanswer1 > countanswer2 and countanswer1 > countanswer3:
-                        choose(answers[0])
-                    elif countanswer2 > countanswer1 and countanswer2 > countanswer3:
-                        choose(answers[1])
-                    elif countanswer3 > countanswer1 and countanswer3 > countanswer2:
-                        choose(answers[2])
-                    else:
-                        print(bad + 'Pick that best answer')
-                elif countanswer1 > countanswer2 and countanswer1 > countanswer3:
-                    choose(answers[0])
-                elif countanswer2 > countanswer1 and countanswer2 > countanswer3:
-                    choose(answers[1])
-                elif countanswer3 > countanswer3 and countanswer3 > countanswer2:
-                    choose(answers[2])
-                else:
-                    print(bad + 'Pick the highest scoring answer')
-                print(hardreturn)
+        if answer1count > answer2count and answer1count > answer3count:
+            choose(answers[0])
+        elif answer2count > answer1count and answer2count > answer3count:
+            choose(answers[1])
+        elif answer3count > answer1count and answer3count > answer2count:
+            choose(answers[2])
+        else:
+            print(bad + 'Pick that best answer')
+    elif answer1count > answer2count and answer1count > answer3count:
+        choose(answers[0])
+    elif answer2count > answer1count and answer2count > answer3count:
+        choose(answers[1])
+    elif answer3count > answer1count and answer3count > answer2count:
+        choose(answers[2])
+    else:
+        print(bad + 'Pick the highest scoring answer')
+    print(hardreturn)
 
 
 if __name__ == "__main__":
     try:
         config = ConfigParser.RawConfigParser()
         config.read(os.path.join(os.path.dirname(__file__), 'bot.cfg'))
-        #config.read(os.path.join(os.path.dirname(__file__), 'bot-523ce4ab538b.cfg'))
         os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = config.get(
             'bot_config', 'GOOGLE_APPLICATION_CREDENTIALS')
 
